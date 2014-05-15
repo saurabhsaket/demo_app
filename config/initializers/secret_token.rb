1afba53b9ac9019fadf9b61f3dd4ab65c8ba4e14
saurabhsaket <saurabhsaket@ymail.com>
2014-05-15 14:15:15 +0530

    Initial Signup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = 'c994d71005ef6f84fd750681980ddc42c789fa12806abda0bf763c246d1692d4138b718d5ded31e1211d9662f732393517ce2a6fb47dc72fc2d87bd7de425396'
+DemoApp::Application.config.secret_key_base = '7f0a19eac1a3a420cc22047a64400da8faca0551042613b6c9e3d86572831582b1e415e16a8e3fd5afa1ff849fdabd14a571d89f7b25913f331b9b010747eac5'
